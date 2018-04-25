2276463e704f7bca761e4cfcce2e6cc20c3715f7
wushihang <285617793@qq.com>
2018-04-25 22:54:09 +0800

    post project files

---
+++ src/components/demo1.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>{{ msg }}</h3>
+</template>
+
+<script>
+export default {
+  name: 'demo1',
+  data () {
+    return {
+      msg: 'Welcome to demo1'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import demo1 from '@/components/demo1'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/demo1',
+      name: 'demo1',
+      component: demo1
+    }
+  ]
+})
